820eeb743cb0a2ef98af8bbf3a8d96219c268d02
Pat Bills <billspat@msu.edu>
2024-01-30 21:43:34 -0500

    add method to get sql engine, don't rely on globals

--- src/ewxpwsdb/db/database.py
+++ src/ewxpwsdb/db/database.py
@@ -17,11 +17,24 @@ def get_db_url(fallback_url = "sqlite:///ewxpws.db" ):
     else:
         return(fallback_url)
 
+def get_engine(db_url = None):
+    """ create an engine, and ask DB not to convert datetimes using timezone of the server, but always assume UTC"""
 
-# create global engine var
-engine = create_engine(url = get_db_url(), echo=True)
+    if not db_url:
+        db_url = get_db_url()
+
+    engine = create_engine(url = db_url, 
+                           echo=True, 
+                           connect_args={"options": "-c timezone=utc"})        
+
+    return engine
+    
+# create global engine var, override as necessary
+engine = get_engine()
 
 def init_db(engine=engine):
+    """create new blank tables etc for the db URLin the engine. 
+    This requires that the database in the URL already exists on the server in the URL"""
     SQLModel.metadata.create_all(engine)
 
 def get_session(engine = engine):
