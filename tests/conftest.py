347139f627360f6d8b9173d06d6fccafa35f3f7a
Pat Bills <billspat@msu.edu>
2024-02-01 20:51:06 -0500

    move db fixures to conftest for use by other test modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
 import pytest
+from sqlmodel import Session
+from sqlalchemy import Engine
+from ewxpwsdb.db.importdata import import_station_file, read_station_table
+from ewxpwsdb.db.database import init_db, create_engine
+
 
 def pytest_addoption(parser):
 
@@ -16,11 +21,74 @@ def pytest_addoption(parser):
                      action='store_true',
                      help='use this to skip importing data (assumes test db already has data)')
 
-from ewxpwsdb.db.importdata import read_station_table
-from ewxpwsdb.db.models import WeatherStation
+
+
+def rm_sqlite_file(db_url):
+    import re
+    sqlite_prefix = 'sqlite:///'
+    if (re.match(sqlite_prefix, db_url)):
+        sqlite_file = db_url.replace(sqlite_prefix, '')
+        try:
+            os.remove(sqlite_file)
+        except Exception as e:
+            print(f"error deleting {sqlite_file}: {e}")
+            return False        
+    else:
+        print(f"not a sqlite url {db_url}, not deleting")
+        return False
+
+    return True
 
 @pytest.fixture(scope = 'module')
 def station_file():
     test_file = 'data/test_stations.tsv'
     return(test_file)
 
+
+@pytest.fixture(scope = 'module')
+def test_db_url(request: pytest.FixtureRequest)->str:
+    return request.config.getoption("--dburl")
+
+@pytest.fixture(scope = 'module')
+def db_engine(test_db_url: str):
+    # need to create new test-only db, 
+    # would like to use a database.py module method rather than sqlmodel code here explicitly 
+    engine = create_engine(url = test_db_url, echo=True)
+    
+    # create the test database
+    init_db(engine)
+
+    yield engine
+    
+    engine.dispose()
+    rm_sqlite_file(test_db_url)
+
+@pytest.fixture(scope = 'module')
+def db_session(db_engine: Engine):
+
+    with Session(db_engine) as session:
+        yield session
+        
+    # session = Session(db_engine)
+    # yield session
+    # # remove any changes made to the db
+    # session.rollback()
+    # session.close()
+
+@pytest.fixture(scope = 'module')
+def test_station_data(request: pytest.FixtureRequest):
+    station_file = request.config.getoption("--file")
+    # check that it exists
+    stations = read_station_table(station_file)
+    return stations
+
+
+@pytest.fixture(scope = 'module')
+def db_with_data(db_engine: Engine, request: pytest.FixtureRequest):
+
+    if not request.config.getoption("--no-import"):   
+        station_file = request.config.getoption("--file")
+        # will raise exception if there is a problem
+        import_station_file(station_file, db_engine)
+    
+    yield db_engine
--- tests/test_sqlmodel_db.py
+++ tests/test_sqlmodel_db.py
@@ -4,76 +4,12 @@ from typing import Any
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import Engine
-import pytest, os
+import pytest
 from pydantic import ValidationError
-from ewxpwsdb.db.database import Session, init_db, engine, get_session, create_engine
-from ewxpwsdb.db.models import WeatherStation, Reading, StationType
-from ewxpwsdb.db.importdata import import_station_file, read_station_table
-
+from ewxpwsdb.db.database import Session
+from ewxpwsdb.db.models import WeatherStation, StationType
 
 from sqlmodel import select
-
-# global var override engine to use test db
-def rm_sqlite_file(db_url):
-    import re
-    sqlite_prefix = 'sqlite:///'
-    if (re.match(sqlite_prefix, db_url)):
-        sqlite_file = db_url.replace(sqlite_prefix, '')
-        try:
-            os.remove(sqlite_file)
-        except Exception as e:
-            print(f"error deleting {sqlite_file}: {e}")
-            return False        
-    else:
-        print(f"not a sqlite url {db_url}, not deleting")
-        return False
-
-    return True
-    
-
-@pytest.fixture(scope = 'module')
-def test_db_url(request: pytest.FixtureRequest)->str:
-    return request.config.getoption("--dburl")
-
-@pytest.fixture(scope = 'module')
-def db_engine(test_db_url: str):
-    # need to create new test-only db, 
-    # would like to use a database.py module method rather than sqlmodel code here explicitly 
-    engine = create_engine(url = test_db_url, echo=True)
-    # create the test database
-    init_db(engine)
-    yield engine
-    engine.dispose()
-    rm_sqlite_file(test_db_url)
-
-
-@pytest.fixture(scope = 'module')
-def db_session(db_engine: Engine):
-    session = Session(db_engine)
-    yield session
-    # remove any changes made to the db
-    session.rollback()
-    session.close()
-
-@pytest.fixture(scope = 'module')
-def test_station_data(request: pytest.FixtureRequest):
-    station_file = request.config.getoption("--file")
-    # check that it exists
-    stations = read_station_table(station_file)
-    return stations
-
-
-@pytest.fixture(scope = 'module')
-def db_with_data(db_engine: Engine, request: pytest.FixtureRequest):
-    if not request.config.getoption("--no-import"):   
-        station_file = request.config.getoption("--file")
-        # will raise exception if there is a problem
-        import_station_file(station_file, db_engine)
-        yield db_engine
-
-        # there is no roll back from this, data just gets imported
-        # BUT could delete all the data if we wanted
-
 
 def test_db_connection(db_engine: Engine):
     """"""
