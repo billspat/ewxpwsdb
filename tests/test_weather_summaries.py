354036ccc19a44b24925836c304c1a46514db364
Pat Bills <billspat@msu.edu>
2025-07-02 12:57:32 -0400

    revamp test of summaries all fields

--- tests/test_weather_summaries.py
+++ tests/test_weather_summaries.py
@@ -98,7 +98,64 @@ def test_daily_summary_can_make_a_list(station_readings: StationReadings):
     
     assert  isinstance(  daily_summaries, list)
     assert len(daily_summaries) > 0  
+
+def test_hourly_summary_fields(station_readings: StationReadings):
+    """test that the hourly sql is running and model is getting 
+    fields.  The pydantic model already checks for types so no need to add specific
+    type checks here.  Just check that the fields are there and not None.
     
+    We don't test if individual sensors counts > 0 because some stations
+    don't have all sensors and some sensors may not have data for a given day.
+
+    Args:
+        station_readings (StationReadings): _description_
+    """
+    yesterday:date = date.today() - timedelta(days = 2)
+    
+    hourly_summaries:list[HourlySummary] = station_readings.hourly_summary(
+                                                local_end_date=yesterday, 
+                                                local_start_date=yesterday
+                                                )
+    
+    assert isinstance( hourly_summaries[0], HourlySummary)
+    hs:HourlySummary = hourly_summaries[0]
+    
+    def check_hourly_summary_field(hs:HourlySummary, field_name: str):
+        logger.debug(f"Checking hourly summary field: {field_name}")
+        
+        assert field_name in hs.model_fields
+        field_value = getattr(hs, field_name, None)
+        assert field_value is not None
+        assert isinstance(field_value, (int, float))
+        
+    for field in [                
+                'atmp_count', 
+                'atmp_avg_hourly', 
+                'atmp_max_hourly', 
+                'atmp_min_hourly', 
+                'relh_max_hourly', 
+                'relh_avg_hourly', 
+                'relh_min_hourly', 
+                'pcpn_count', 
+                'pcpn_total_hourly', 
+                'lws_count', 
+                'lws_wet_hourly', 
+                'lws_pwet_hourly', 
+                'wdir_avg_hourly', 
+                'wdir_sdv_hourly', 
+                'wspd_avg_hourly', 
+                'wspd_max_hourly'
+                ]:
+        # summarie fields are named like 'atmp_avg_hourly' so we can get the sensor name
+        # from the field name.  
+        sensor_name = field.split('_')[0]
+        if sensor_name in station_readings.weather_api.supported_variables:
+            check_hourly_summary_field(hs, field)
+                
+    assert hs.api_hourly_frequency == station_readings.weather_api.expected_hourly_frequency
+
+    
+        
 def test_daily_summary_fields(station_readings: StationReadings):
     """test that the daily sql is running and model is getting 
     fields.  The pydantic model already checks for types so no need to add specific
@@ -139,16 +196,9 @@ def test_daily_summary_fields(station_readings: StationReadings):
                   'lws_count',
                   'wspd_avg_daily',
                   'wspd_count']:
-        if field in station_readings.weather_api.supported_variables:
+        sensor_name = field.split('_')[0]
+        if sensor_name in station_readings.weather_api.supported_variables:            
             check_daily_summary_field(ds, field)
-                
-    # assert 'atmp_avg_daily' in ds.model_fields and ds.atmp_avg_daily is not None
-    # assert isinstance(ds['atmp_avg_daily'], float)
-    # assert isinstance(ds['atmp_count'], int) and ds.atmp_count is not None
-    
-    # assert 'relh_avg_daily' in ds.model_fields and ds.relh_avg_daily is not None
-    # assert isinstance(ds.relh_avg_daily, float)
-    # assert isinstance(ds.relh_count, int) and ds.relh_count is not None
     
     assert ds.api_daily_frequency == station_readings.weather_api.expected_daily_frequency
     assert ds.record_count > 0
